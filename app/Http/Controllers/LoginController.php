1ab9cc58b6e3b4591b79d34d5d7252238ed551c2
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-11-03 21:22:35 +0900

    Enabled SPA Authentication.

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if (!$user) {
+                return response('', 500);
+            }
+            $request->session()->regenerate();
+            return response()->json(['message' => 'Logged in.']);
+        }
+
+        return response()->json(['message' => 'Failed to login.'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return response()->json(['message' => 'You successfully logout.'], 200);
+    }
+}
